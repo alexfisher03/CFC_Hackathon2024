c26078386f54755aed8b8f11420d5809572ac27b
alexfisher03 <fisherr.alexander@gmail.com>
2024-02-10 12:45:06 -0500

    creditspotter logo added

--- financial-advisor-chatbot/src/App.js
+++ financial-advisor-chatbot/src/App.js
@@ -1,17 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-}
 function MyForm() {
   return (
     <form>
-      <label>Enter your name:
+      <label>
+        Enter your name:
         <input type="text" />
       </label>
     </form>
-  )
+  );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 export default App;
