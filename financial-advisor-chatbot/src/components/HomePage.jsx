719d3cbfbe6e47182cceec3006e0010f257858b3
alexfisher03 <fisherr.alexander@gmail.com>
2024-02-10 15:08:37 -0500

    InputField complete 1

--- financial-advisor-chatbot/src/components/HomePage.jsx
+++ financial-advisor-chatbot/src/components/HomePage.jsx
@@ -1,37 +1,58 @@
-import React from 'react';
-import image from "../assets/CredditSpotterLogo.png"
-
-
-
+import React from "react";
+import image from "../assets/CredditSpotterLogo.png";
 
 const HomePage = () => {
   return (
-  <body>
-    <div className="fixed w-full h-[215px] flex justify-around items-center px-4 bg-[#091824] text-grey-300">
-      <img src={image} style={{width: '400px'}}/>
-      <p className="text-left md:text-center">
-        <h1 class="text-white">Created by:</h1>
-        <div class="name_credit">
-          <ul className="text-lg font-bold">
-            <li>
-              <a class="text-white hover:underline" href="https://github.com/alexfisher03" target="_blank">Alex</a>  
-            </li>
-            <li>
-              <a class="text-white hover:underline " href="https://github.com/Maykur" target="_blank">Julian</a>  
-            </li>
-            <li>
-              <a class="text-white hover:underline" href="https://github.com/Leo-Graham" target="_blank">Leo</a>  
-            </li>
-            <li>
-              <a class="text-white hover:underline" href="https://github.com/j-frankel" target="_blank">Jacob</a>  
-            </li>
-          </ul>
-        </div>
-      </p>
-    </div>
-  </body>
+    <body>
+      <div className="fixed w-full h-[150px] flex justify-around items-center px-4 bg-[#091824] text-grey-300">
+        <img src={image} style={{ width: "400px" }} />
+        <p className="text-left md:text-center">
+          <h1 class="text-white">Created by:</h1>
+          <div class="name_credit">
+            <ul className="text-lg font-bold">
+              <li>
+                <a
+                  class="text-white hover:underline"
+                  href="https://github.com/alexfisher03"
+                  target="_blank"
+                >
+                  Alex
+                </a>
+              </li>
+              <li>
+                <a
+                  class="text-white hover:underline "
+                  href="https://github.com/Maykur"
+                  target="_blank"
+                >
+                  Julian
+                </a>
+              </li>
+              <li>
+                <a
+                  class="text-white hover:underline"
+                  href="https://github.com/Leo-Graham"
+                  target="_blank"
+                >
+                  Leo
+                </a>
+              </li>
+              <li>
+                <a
+                  class="text-white hover:underline"
+                  href="https://github.com/j-frankel"
+                  target="_blank"
+                >
+                  Jacob
+                </a>
+              </li>
+            </ul>
+          </div>
+        </p>
+      </div>
+      <div className="h-[150px]"></div>
+    </body>
   );
 };
-
 
 export default HomePage;
--- financial-advisor-chatbot/src/components/InputField.jsx
+++ financial-advisor-chatbot/src/components/InputField.jsx
@@ -1,10 +1,121 @@
 import React from "react";
-import { Input } from "@material-tailwind/react";
 
 export default function InputField() {
   return (
-    <div className="w-72">
-      <Input label="Username" />
+    <div class="w-full fixed h-screen bg-[#091824]">
+      {/*container*/}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-[-150px] pt-10">
+        <form>
+          <div class="grid gap-6 mb-6 md:grid-cols-2">
+            <div>
+              <label
+                for="Name"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Name
+              </label>
+              <input
+                type="text"
+                id="Name"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Enter your name..."
+                required
+              />
+            </div>
+            <div>
+              <label
+                for="creditscore"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Credit Score
+              </label>
+              <input
+                type="number"
+                id="creditscore"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="300-850"
+                required
+              />
+            </div>
+            <div>
+              <label
+                for="income"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Income
+              </label>
+              <input
+                type="number"
+                id="income"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="3000"
+                required
+              />
+            </div>
+            <div>
+              <label
+                for="expenses"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Expenses
+              </label>
+              <input
+                type="number"
+                id="expenses"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="1500"
+                required
+              />
+            </div>
+          </div>
+          <div class="mb-6">
+            <label
+              for="goal"
+              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Enter your financial goal(s):
+            </label>
+            <input
+              type="text"
+              id="goal"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="How can I increase my credit score?"
+              required
+            />
+          </div>
+
+          <div class="flex items-start mb-6">
+            <div class="flex items-center h-5">
+              <input
+                id="remember"
+                type="checkbox"
+                value=""
+                class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+                required
+              />
+            </div>
+            <label
+              for="remember"
+              class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              I agree with the{" "}
+              <a
+                href="#"
+                class="text-blue-600 hover:underline dark:text-blue-500"
+              >
+                terms and conditions
+              </a>
+              .
+            </label>
+          </div>
+          <button
+            type="submit"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
